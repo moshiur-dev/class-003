17780bd59eaca242bcf7e2999ce4179f6f7eb938
MD. Moshiur Rahman <moshiurfr@users.noreply.github.com>
2020-11-28 12:33:23 +0600

    Add files via upload
    jakono

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        margin: 20,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1000,
+        dots:true
+
+    });
+});
